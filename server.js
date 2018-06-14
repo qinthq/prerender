a39f0521e38eaf7da6c0e1a8754adaef80df0c11
pvsune <pvsune@hotmail.com>
2018-06-14 12:43:44 +0800

    Install prerender-memory-cache for caching rendered pages.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
-
+var prerender = require('prerender');
 var server = prerender();
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-
+server.use(require('prerender-memory-cache'))
 server.start();
